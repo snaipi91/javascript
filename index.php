2482edd60eeee9b043f6a05ecb01e20e90ff6cd8
johndoe <snaipi91@rambler.ru>
2014-09-15 21:45:06 +0400

    .Изменения

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+/**
+ * Created by PhpStorm.
+ * User: Andrey
+ * Date: 15.09.14
+ * Time: 17:46
+ */
+
+//$res = 31.320;
+
+echo "Вы успешно зашли на наш сайт" . " " . $res;
