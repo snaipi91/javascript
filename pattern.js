bee59b1d58fca5f7dd182057a898e8cda0b3dfc3
Andrey <snaipi91@rambler.ru>
2015-03-04 21:57:22 +0400

    Update pattern.js

--- pattern.js
+++ pattern.js
@@ -30,3 +30,27 @@ var module = (function() {
 }());
 
 module.facade({run:true, val:10}); // Текущее значение: 10, процесс запущен
+
+//Singleton 
+var Singleton_B;
+(function(){
+    var instance;
+    var anticlone_proxy;
+
+    Singleton_B = function(){
+        if( instance ){ return instance; }
+
+        instance = 
+        {
+            _counter: 0,
+            log: function( text ){ this._counter++; console.log( text + this._counter ); }
+        }
+
+        anticlone_proxy =
+        {
+            log: function( text ){ return instance.log( text ); }
+        }
+
+        return anticlone_proxy;
+    };
+})();
